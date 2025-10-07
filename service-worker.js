6d9697b0fc020f94b099233ea68d45d866c1bb43
Francisco <frank1989pe@gmail.com>
2025-10-07 18:08:52 -0400

    version mejorada del sW

--- service-worker.js
+++ service-worker.js
@@ -15,17 +15,50 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting()) // Activar inmediatamente
     );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
+    // Para HTML, intentar red primero
+    if (event.request.url.includes('.html') || event.request.url.endsWith('/')) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const responseClone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
                     return response;
-                }
-                return fetch(event.request);
-            })
+                })
+                .catch(() => caches.match(event.request))
+        );
+    } else {
+        // Para otros archivos, caché primero
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => response || fetch(event.request))
+        );
+    }
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Eliminando caché antigua:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Tomar control inmediatamente
     );
+});
+
+self.addEventListener('message', event => {
+    if (event.data && event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
 });
